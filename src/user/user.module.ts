da3cd5a796053df49165fdd5eef5b052d08c668c
Do Trong Duc <ducdotrong2000@gmail.com>
2022-10-28 23:58:02 +0700

    fix: config swagger and add user repo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
+import { UserRepository } from './repositories/user.repository';
 @Module({
   imports: [ConfigModule, TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  exports: [UserService],
-  providers: [UserService],
+  exports: [UserService, UserRepository],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
 
