57115f11030742b01e613bc5a4082fbe3a86cd59
Do Trong Duc <ducdotrong2000@gmail.com>
2022-10-29 00:01:45 +0700

    feat: add open api for controller

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,6 +1,14 @@
-import {  Controller } from "@nestjs/common";
-import { UserService } from "../services/user.service";
-@Controller("users")
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserService } from '../services/user.service';
+@Controller('users')
+@ApiTags('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('hello')
+  async helloWorld(): Promise<string> {
+    return 'hello';
+  }
 }
+
